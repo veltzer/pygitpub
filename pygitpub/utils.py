01d2a0a5cb1ce804c41aa88fdf32a05ba243c734
Mark Veltzer <mark.veltzer@gmail.com>
2022-04-28 02:42:37 +0300

    first command implemented

--- pygitpub/utils.py
+++ pygitpub/utils.py
@@ -1,5 +1,7 @@
 import logging
 import os
+import configparser
+import github
 
 from pygitpub import LOGGER_NAME
 
@@ -13,3 +15,15 @@ def get_number_of_files(folder: str) -> int:
     for _root, _directories, files in os.walk(folder):
         count += len(files)
     return count
+
+
+def github_login():
+    inifile = os.path.expanduser("~/.details.ini")
+    config = configparser.ConfigParser()
+    config.read(inifile)
+    # all of our github secret info
+    # opt_username = config.get("github", "username")
+    # opt_password = config.get("github", "password")
+    opt_personal_token = config.get("github", "personal_token")
+    g = github.Github(login_or_token=opt_personal_token)
+    return g
