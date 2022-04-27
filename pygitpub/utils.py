02fb1f24d3b1d9ff7b6cfd42cc2a81d35915b0e5
Mark Veltzer <mark.veltzer@gmail.com>
2022-04-28 02:52:11 +0300

    token now in config file

--- pygitpub/utils.py
+++ pygitpub/utils.py
@@ -15,15 +15,3 @@ def get_number_of_files(folder: str) -> int:
     for _root, _directories, files in os.walk(folder):
         count += len(files)
     return count
-
-
-def github_login():
-    inifile = os.path.expanduser("~/.details.ini")
-    config = configparser.ConfigParser()
-    config.read(inifile)
-    # all of our github secret info
-    # opt_username = config.get("github", "username")
-    # opt_password = config.get("github", "password")
-    opt_personal_token = config.get("github", "personal_token")
-    g = github.Github(login_or_token=opt_personal_token)
-    return g
