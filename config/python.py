4bc0f2437c4a87f0cae7a0ed34a246f1bc19e643
Mark Veltzer <mark.veltzer@gmail.com>
2022-04-28 02:26:52 +0300

    first commit

---
+++ config/python.py
@@ -0,0 +1,44 @@
+import config.project
+
+package_name = config.project.project_name
+
+console_scripts = [
+    'pygitpub=pygitpub.main:main',
+]
+
+setup_requires = [
+]
+
+run_requires = [
+    'pytconf',
+    'pylogconf',
+    'tqdm',
+    'lmdb',
+]
+
+test_requires = [
+    'pylint',
+    'pytest',
+    'pytest-cov',
+    'flake8',
+    'pymakehelper',
+]
+
+dev_requires = [
+    'pyclassifiers',
+    'pypitools',
+    'pydmt',
+    'Sphinx',
+    'pyyaml',
+]
+
+install_requires = list(setup_requires)
+install_requires.extend(run_requires)
+
+python_requires = ">=3.9"
+
+extras_require = {
+}
+test_os = "[ubuntu-18.04, ubuntu-20.04]"
+test_python = "[3.9]"
+test_container = "[ 'ubuntu:18.04', 'ubuntu:20.04' ]"
