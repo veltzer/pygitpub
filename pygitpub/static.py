e68318f1f49b77b5584ae53d80d3a27eb650c96a
Mark Veltzer <mark.veltzer@gmail.com>
2022-04-28 02:49:53 +0300

    new version

--- pygitpub/static.py
+++ pygitpub/static.py
@@ -1,5 +1,5 @@
 """ version which can be consumed from within the module """
-VERSION_STR = "0.0.2"
+VERSION_STR = "0.0.3"
 DESCRIPTION = "help you work with github"
 APP_NAME = "pygitpub"
 LOGGER_NAME = "pygitpub"
