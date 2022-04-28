13de72a4c80800560450d838f2137428d2f4c9cf
Mark Veltzer <mark.veltzer@gmail.com>
2022-04-28 05:29:14 +0300

    new version

--- README.rst
+++ README.rst
@@ -12,6 +12,6 @@ project website: https://veltzer.github.io/pygitpub
 
 author: Mark Veltzer
 
-version: 0.0.15
+version: 0.0.16
 
 
--- pygitpub/static.py
+++ pygitpub/static.py
@@ -1,5 +1,5 @@
 """ version which can be consumed from within the module """
-VERSION_STR = "0.0.15"
+VERSION_STR = "0.0.16"
 DESCRIPTION = "help you work with github"
 APP_NAME = "pygitpub"
 LOGGER_NAME = "pygitpub"
