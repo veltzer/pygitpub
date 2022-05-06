e46e70b18550db15c8b19e59f47ba518e2a2fe78
Mark Veltzer <mark.veltzer@gmail.com>
2022-05-06 14:11:49 +0300

    better deps

--- config/python.py
+++ config/python.py
@@ -3,39 +3,30 @@ import config.project
 package_name = config.project.project_name
 
 console_scripts = [
-    'pygitpub=pygitpub.main:main',
-]
-
-setup_requires = [
+    "pygitpub=pygitpub.main:main",
 ]
 
 run_requires = [
-    'pytconf',
-    'pylogconf',
-    'PyGithub',
+    "pytconf",
+    "pylogconf",
+    "PyGithub",
 ]
 
 test_requires = [
-    'pylint',
-    'pytest',
-    'pytest-cov',
-    'flake8',
-    'pymakehelper',
+    "pylint",
+    "pytest",
+    "pytest-cov",
+    "flake8",
+    "pymakehelper",
 ]
 
 dev_requires = [
-    'pyclassifiers',
-    'pypitools',
-    'pydmt',
-    'Sphinx',
-    'pyyaml',
+    "pyclassifiers",
+    "pypitools",
+    "pydmt",
+    "Sphinx",
+    "pyyaml",
 ]
-
-install_requires = list(setup_requires)
-install_requires.extend(run_requires)
-
-extras_require = {
-}
 
 python_requires = ">=3.9"
 test_os = ["ubuntu-20.04"]
