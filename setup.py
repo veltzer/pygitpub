1a55f9a2f3bb63ce64d93ebc999b669e0c19f85b
Mark Veltzer <mark.veltzer@gmail.com>
2022-11-01 16:11:16 +0200

    new version

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ def get_readme():
 setuptools.setup(
     # the first three fields are a must according to the documentation
     name="pygitpub",
-    version="0.0.38",
+    version="0.0.39",
     packages=[
         'pygitpub',
     ],
