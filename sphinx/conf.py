76975504768bc6d45c87c0aa3983ca89aa52828d
Mark Veltzer <mark.veltzer@gmail.com>
2023-04-28 17:38:36 +0300

    sphinx

--- sphinx/conf.py
+++ sphinx/conf.py
@@ -2,8 +2,17 @@ extensions = ['sphinx.ext.autodoc',
               'sphinx.ext.coverage',
               'sphinx.ext.viewcode']
 
+
 project = 'pygitpub'
 project_copyright = '2020, Mark Veltzer'
 author = 'Mark Veltzer'
-version = '0.0.1'
+import config.version
+version = ".".join(str(x) for x in config.version.tup)
 release = '0.0.1'
+
+html_theme_options = {
+        "show_powered_by": "false",
+}
+rst_epilog = f'''
+.. |project| replace:: {project}
+'''
