da12fd7b77d400288ed0d4216d3816f7ef31391a
Mark Veltzer <mark.veltzer@gmail.com>
2023-11-18 12:12:18 +0200

    new version

--- pygitpub/static.py
+++ pygitpub/static.py
@@ -1,5 +1,5 @@
 """ version which can be consumed from within the module """
-VERSION_STR = "0.0.48"
+VERSION_STR = "0.0.49"
 DESCRIPTION = "Pygitpub will help you work with github"
 APP_NAME = "pygitpub"
 LOGGER_NAME = "pygitpub"
--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ def get_readme():
 setuptools.setup(
     # the first three fields are a must according to the documentation
     name="pygitpub",
-    version="0.0.48",
+    version="0.0.49",
     packages=[
         "pygitpub",
         "pygitpub.utils",
