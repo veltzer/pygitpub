aca60b8405fb8f8849f626b38d445a23160cb42c
Mark Veltzer <mark.veltzer@gmail.com>
2025-06-28 21:46:17 +0300

    more

--- config/python.py
+++ config/python.py
@@ -1,8 +1,8 @@
 """ python deps for this project """
 
-console_scripts: list[str] = [
-    "pygitpub=pygitpub.main:main",
-]
+scripts: dict[str,str] = {
+    "pygitpub": "pygitpub.main:main",
+}
 
 config_requires: list[str] = [
     "pyclassifiers",
